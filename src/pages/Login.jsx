cef4622ffb85ac4c0fe9ae2799b8d457733e6e8b
ismailasylla <ismailasyllaa@gmail.com>
2022-05-08 14:26:26 +0400

    Login form...

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const Login = () => {
+  const [studentData, setStudentData] = useState({
+    name: "",
+  });
+
+  const { name } = studentData;
+
+  const onChange = (e) => {
+    setStudentData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container">
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Search Student</Form.Label>
-          <Form.Control type="name" placeholder="Enter Student Name" />
+          <Form.Control
+            type="text"
+            id="name"
+            placeholder="Enter Student Name"
+            onChange={onChange}
+            value={name}
+            name="name"
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
